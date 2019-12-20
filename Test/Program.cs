083eabdb70a75dfd487474da7bfbef2f03100a6d
峻廷 林 <sanyo7931@yahoo.com.tw>
2019-12-20 09:58:01 +0800

    測試

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,44 +6,77 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            int x;
-            double z;
-            string input= Console.ReadLine();
-            string Inty = "";
-            string Doubley = "";
-            string[] inputArray = input.Split('.');
-            if (inputArray.Length==2)
+            int inputInt = 0;
+            double inputDouble = 0.0;
+            string input = "";
+            string[] inputArray;
+            Console.WriteLine("10進制轉2進制");
+            Console.Write("輸入數字: ");
+            input = Console.ReadLine();
+            inputArray = input.Split('.');
+            try
             {
-                x = Convert.ToInt32(inputArray[0]);
-                z = Convert.ToDouble(inputArray[1].Insert(0, "0."));
-            }
-            else
-            {
-                x = Convert.ToInt32(inputArray[0]);
-                z = 0;
-            }
-
-
-            while(x!=0)
-            {
-                Inty = Inty.Insert(0,Convert.ToString(x % 2));
-                x = x / 2;
-            }
-            for (int i=0;i<50;i++)
-            { 
-                z = z * 2;
-                string[] DoubleyArray = Convert.ToString(z).Split('.');
-                Doubley = Doubley + DoubleyArray[0];
-                if (DoubleyArray.Length==2)
+                if (inputArray.Length == 2)
                 {
-                    z = Convert.ToDouble(DoubleyArray[1].Insert(0, "0."));
+                    inputInt = Convert.ToInt32(inputArray[0]);
+                    inputDouble = Convert.ToDouble(inputArray[1].Insert(0, "0."));
+                    Console.WriteLine(input + " = " + BinaryInt(inputInt) + "." + BinaryDouble(inputDouble));
+                }
+                else if (inputArray.Length == 1)
+                {
+                    inputInt = Convert.ToInt32(inputArray[0]);
+                    Console.WriteLine(input + " = " + BinaryInt(inputInt));
+                }
+                else
+                {
+                    Console.WriteLine("輸入錯誤");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("輸入錯誤");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("輸入錯誤");
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+        static string BinaryInt(int inputInt)
+        {
+            string binaryInt = "";
+            if (inputInt == 0)
+            {
+                return "0";
+            }
+            while (inputInt != 0)
+            {
+                binaryInt = binaryInt.Insert(0, Convert.ToString(inputInt % 2));
+                inputInt /= 2;
+            }
+            return binaryInt;
+        }
+        static string BinaryDouble(double inputDouble)
+        {
+            string binaryDouble = "";
+            for (int i = 0; i < 50; i++)
+            {
+                inputDouble *= 2;
+                string[] binaryDoubleArray = Convert.ToString(inputDouble).Split('.');
+                binaryDouble += binaryDoubleArray[0];
+                if (binaryDoubleArray.Length == 2)
+                {
+                    inputDouble = Convert.ToDouble(binaryDoubleArray[1].Insert(0, "0."));
                 }
                 else
                 {
                     break;
                 }
             }
-            Console.WriteLine(Inty+"."+Doubley);
+            return binaryDouble;
         }
     }
 }
